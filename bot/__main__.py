5c192c5716984293a1a38c321bfeb66d7faa853d
xalienxx <95128665+xalienxx@users.noreply.github.com>
2022-02-12 17:07:34 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -56,7 +56,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
         sendMarkup(start_string, context.bot, update, reply_markup)
     else:
         sendMarkup(
-            'Oops! not a Authorized user.🙂\nPM only works for Owner.</b>.',
+            'Oops! not a Authorized user.🙂\nPM only works for admin.</b>.',
             context.bot,
             update,
             reply_markup,
